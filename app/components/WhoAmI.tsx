2dfd30d0606802528e1faf34e1d48a7856e01666
Eric <ericnie12310@gmail.com>
2025-03-09 19:10:21 -0400

    name

--- app/components/WhoAmI.tsx
+++ app/components/WhoAmI.tsx
@@ -3,8 +3,8 @@ export default function WhoAmI() {
     <div className="bg-[#202020] p-6 rounded-lg">
       <h1 className="text-2xl font-bold mb-4">Who am I?</h1>
       <p className="text-gray-300">
-        Hey! My Name is Tim Gatzke, I am a Full Stack Developer and I am currently a
-        student in Germany. I am passionate about creating software solutions and I am
+        Hey! My Name is Eric Nie, I am a Full Stack Developer and I am currently a
+        student at University of Waterloo. I am passionate about creating software solutions and I am
         always looking for new challenges. If you have a project that you would like to
         work on, feel free to contact me.
       </p>
