f7a167feb352c87aa7e44c861a8e0ba36029e833
Eric <ericnie12310@gmail.com>
2025-03-09 18:58:12 -0400

    Fix quote entities error

--- app/components/Quote.tsx
+++ app/components/Quote.tsx
@@ -2,7 +2,7 @@ export default function Quote() {
   return (
     <div className="bg-[#202020] p-6 rounded-lg">
       <blockquote className="text-gray-300 italic">
-        "Your favorite programming quote here"
+        &ldquo;Your favorite programming quote here&rdquo;
       </blockquote>
       <p className="text-gray-400 mt-2">- Author</p>
     </div>
